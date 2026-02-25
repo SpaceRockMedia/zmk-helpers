4ce5386e849a81cee75f492a4e974364d9258e65
Shawn McElroy <shawn.mcelroy@progyny.com>
2026-02-25 11:38:12 -0800

    add 50 key for apiaster

---
+++ include/zmk-helpers/key-labels/50.h
@@ -0,0 +1,71 @@
+/*                                            50 KEY / LAYOUT MAPPING
+
+  ╭────────────────────────┬────────────────────────╮ ╭─────────────────────────┬─────────────────────────╮
+  │      0   1   2   3   4 │  5   6   7   8   9     │ │     LN4 LN3 LN2 LN1 LN0 │ RN0 RN1 RN2 RN3 RN4     │
+  │     10  11  12  13  14 │ 15  16  17  18  19     │ │     LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4     │
+  │ 20  21  22  23  24  25 │ 26  27  28  29  30  31 │ │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
+  │ 32  33  34  35  36  37 │ 38  39  40  41  42  43 │ │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
+  │             44  45  46 │ 47  48  49             │ │             LH2 LH1 LH0 │ RH0 RH1 RH2             │
+  ╰────────────────────────┴────────────────────────╯ ╰─────────────────────────┴─────────────────────────╯ */
+
+#pragma once
+
+#define LN0  4  // left-number row
+#define LN1  3
+#define LN2  2
+#define LN3  1
+#define LN4  0
+
+#define RN0  5  // right-number row
+#define RN1  6
+#define RN2  7
+#define RN3  8
+#define RN4  9
+
+#define LT0 14  // left-top row
+#define LT1 13
+#define LT2 12
+#define LT3 11
+#define LT4 10
+
+#define RT0 14  // right-top row
+#define RT1 16
+#define RT2 17
+#define RT3 18
+#define RT4 19
+
+#define LM0 25  // left-middle row
+#define LM1 24
+#define LM2 23
+#define LM3 22
+#define LM4 21
+#define LM5 20
+
+#define RM0 26  // right-middle row
+#define RM1 27
+#define RM2 28
+#define RM3 29
+#define RM4 30
+#define RM5 31
+
+#define LB0 37  // left-bottom row
+#define LB1 36
+#define LB2 35
+#define LB3 34
+#define LB4 33
+#define LB5 32
+
+#define RB0 38  // right-bottom row
+#define RB1 39
+#define RB2 40
+#define RB3 41
+#define RB4 42
+#define RB5 43
+
+#define LH0 46  // left thumb keys
+#define LH1 44
+#define LH2 44
+
+#define RH0 47  // right thumb keys
+#define RH1 48
+#define RH2 49
